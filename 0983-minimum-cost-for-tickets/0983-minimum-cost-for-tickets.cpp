7cc55abae99a3a055939f31bb9acbaa439bcdb76
rachit2905 <85569226+rachit2905@users.noreply.github.com>
2023-01-03 22:05:49 +0530

    Time: 3 ms (87.64%), Space: 9.2 MB (99.63%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
-    int mincostTickets(vector<int>& DAYS, vector<int>& cost) {
-         unordered_set<int> days(DAYS.begin(),DAYS.end());
-        vector<int> dp(367);
-        
-        for(int i=1 ; i<366 ; i++)
-        {
-            dp[i] = dp[i-1];
-            if(days.find(i) != days.end())
-            {
-			// cost of ith day , decision of ith day will be
-                dp[i] = min({ dp[i - 1] + cost[0],   // 1st case
-                             dp[max(0, i - 7)] + cost[1],  // 2nd case
-                             dp[max(0, i - 30)] + cost[2]});  // 3rd case
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+            int last = days.back(), dp[last + 1];
+        dp[0] = 0;
+        for (int i = 1, j = 0; i <= last; i++) {
+            if (i == days[j]) {
+                // moving j to the next number
+                j++;
+                // dp[i] must be the min of
+                dp[i] = min({
+                    // a daily ticket plus previous costs
+                    costs[0] + dp[i - 1],
+                    // a weekly ticket plus previous costs
+                    costs[1] + dp[i - min(i, 7)],
+                    // a monthly ticket plus previous costs
+                    costs[2] + dp[i - min(i, 30)],
+                });
+            }
+            else {
+                // alternatively we keep progressing with the last recorded value
+                dp[i] = dp[i - 1];
             }
         }
-        return dp[365];
+        return dp[last];
     }
+
 };

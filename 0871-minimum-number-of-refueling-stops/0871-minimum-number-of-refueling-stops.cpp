e6663844075062a2d0212c0183496e97d8a45b3f
rachit2905 <85569226+rachit2905@users.noreply.github.com>
2023-01-03 19:13:32 +0530

    Time: 67 ms (47.66%), Space: 16.3 MB (57.77%) - LeetHub

--- 0871-minimum-number-of-refueling-stops/0871-minimum-number-of-refueling-stops.cpp
+++ 0871-minimum-number-of-refueling-stops/0871-minimum-number-of-refueling-stops.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
-    int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
-        int n=stations.size();
-        vector<long long int>dp(n+1,0);
-        dp[0]=startFuel;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i;j>=0 && dp[j]>=stations[i][0];j--)
-            {
-                dp[j+1]=max(dp[j+1],dp[j]+stations[i][1]);
-            }
+    int minRefuelStops(int target, int cur, vector<vector<int>>& s) {
+       int i = 0, res;
+        priority_queue<int>pq;
+        for (res = 0; cur < target; res++) {
+            while (i < s.size() && s[i][0] <= cur)
+                pq.push(s[i++][1]);
+            if (pq.empty()) return -1;
+            cur += pq.top(), pq.pop();
         }
-        for(int i=0;i<=n;i++)
-        {
-            if(dp[i]>=target)return i;
-        }
-        return -1;
+        return res;
     }
 };

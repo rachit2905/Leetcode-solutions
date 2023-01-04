52388bbef2166ea1f305dad8ded4765f355caf9d
rachit2905 <85569226+rachit2905@users.noreply.github.com>
2023-01-04 19:34:00 +0530

    Time: 15 ms (16.88%), Space: 11.5 MB (45.35%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& Arr) {
-         int n = Arr.size();
-    vector<vector<int>> dp(n+2,vector<int>(2,0));
-    
-    for(int ind = n-1; ind>=0; ind--){
-        for(int buy=0; buy<=1; buy++){
-            int profit;
-            
-            if(buy==0){// We can buy the stock
-                profit = max(0+dp[ind+1][0], -Arr[ind] + dp[ind+1][1]);
-            }
-    
-            if(buy==1){// We can sell the stock
-                profit = max(0+dp[ind+1][1], Arr[ind] + dp[ind+2][0]);
-            }
-            
-            dp[ind][buy] = profit;
+    int dp[5001][2];
+    int helper(vector<int> &prices, int curr, int n, bool hadBought){
+        if(curr>=n){
+            return 0;
         }
+        if(dp[curr][hadBought]!=-1){
+            return dp[curr][hadBought];
+        }
+        int profit=0;
+        
+        if(hadBought==false){
+            int BUY=(-prices[curr])+helper(prices,curr+1,n,true);  // -ve price because buying takes money out of pocket
+            int NOT_BUY=helper(prices,curr+1,n,hadBought);
+            profit=max(BUY,NOT_BUY);
+        }
+        
+        else{
+            int SELL=prices[curr]+helper(prices,curr+2,n,false);
+            int NOT_SELL=helper(prices,curr+1,n,hadBought);
+            profit=max(SELL,NOT_SELL);
+        }
+        return dp[curr][hadBought]=profit;
     }
     
-    return dp[0][0];
+    int maxProfit(vector<int>& prices){
+        memset(dp,-1,sizeof(dp));
+        int n=prices.size();
+        return helper(prices,0,n,false);
     }
 };

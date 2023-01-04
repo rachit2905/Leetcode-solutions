0bf5bdce7d7408909bc7e6757635b4007927f6f2
rachit2905 <85569226+rachit2905@users.noreply.github.com>
2023-01-04 18:45:27 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (79.52%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int dp[nums.size()+1];
-        memset(dp,0,sizeof(dp));
-        dp[0]=0;
-        dp[1]=nums[0];
+//         int dp[nums.size()+1];
+//         memset(dp,0,sizeof(dp));
+//         dp[0]=0;
+//         dp[1]=nums[0];
+//         for(int i=2;i<=nums.size();i++)
+//         {
+// dp[i]=max(dp[i-1],nums[i-1]+dp[i-2]);
+//         }
+//         return dp[nums.size()];
+        int x=0,y=nums[0],ans=max(x,y);
         for(int i=2;i<=nums.size();i++)
         {
-dp[i]=max(dp[i-1],nums[i-1]+dp[i-2]);
+            ans=max(y,x+nums[i-1]);
+            x=y;
+            y=ans;
         }
-        return dp[nums.size()];
+        return max(x,y);
     }
 };

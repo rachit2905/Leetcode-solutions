f897b8ac1b1fb361860c65b832f9dc8f4c046c52
rachit2905 <85569226+rachit2905@users.noreply.github.com>
2023-01-05 19:31:02 +0530

    Time: 18 ms (75.49%), Space: 12.1 MB (42.49%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,14 +1,35 @@
 class Solution {
 public:
-    int findTargetSumWays(vector<int>& nums, int S) {
-    int n = nums.size();
-        vector<unordered_map<int,int>> dp(n+1);
-        dp[0][0]=1;
-        for(int i=0;i<n;i++)
-            for(auto &p:dp[i]) {
-                dp[i+1][p.first+nums[i]] += p.second; 
-                dp[i+1][p.first-nums[i]] += p.second;
-            }
-        return dp[n][S];
+    int findTargetSumWays(vector<int>& num, int targets) {
+            int totSum = 0;
+         int n=num.size();
+    for(int i=0; i<n;i++){
+        totSum += num[i];
+    }
+    
+    //Checking for edge cases
+    if(totSum-targets <0 || (totSum-targets)%2 ) return 0;
+        int tar=(totSum-targets)/2;
+        vector<vector<int>> dp(n,vector<int>(tar+1,0));
+    
+    if(num[0] == 0) dp[0][0] =2;  // 2 cases -pick and not pick
+    else dp[0][0] = 1;  // 1 case - not pick
+    
+    if(num[0]!=0 && num[0]<=tar) dp[0][num[0]] = 1;  // 1 case -pick
+    
+    for(int ind = 1; ind<n; ind++){
+        for(int target= 0; target<=tar; target++){
+            
+            int notTaken = dp[ind-1][target];
+    
+            int taken = 0;
+                if(num[ind]<=target)
+                    taken = dp[ind-1][target-num[ind]];
+        
+            dp[ind][target]= (notTaken + taken);
+        }
+    }
+    return dp[n-1][tar];
+    
     }
 };

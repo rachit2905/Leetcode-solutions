ab815da1ce61db70efeb21262b7ab4522b10ac07
rachit2905 <85569226+rachit2905@users.noreply.github.com>
2023-01-07 23:13:42 +0530

    Time: 64 ms (48.05%), Space: 6.6 MB (80.52%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int numRollsToTarget(int n, int k, int target) {
-        vector<int>dp(target+1);
-        dp[0]=1;
-        for(int i=1;i<=n;i++)
-        {
-            vector<int>dp1(target+1);
-            for(int j=1;j<=k;j++)
-            {
-                for(int k=j;k<=target;k++)
-                {
-                    dp1[k]=(dp1[k]+dp[k-j])%1000000007;
-                }
-                
+         vector<int> DP(target + 1, 0);
+    DP[0] = 1;
+    for (int i = 1; i <= n; i++) {
+        for (int j = target; j >= 0; j--) {
+            DP[j] = 0;
+            for (int x = 1; x <= k; x++) {
+                if (x <= j)
+                    DP[j] = (DP[j] + DP[j - x]) % (int)(1e9 + 7);
             }
-            swap(dp,dp1);
         }
-        return dp[target];
+    }
+    return DP[target];
     }
 };

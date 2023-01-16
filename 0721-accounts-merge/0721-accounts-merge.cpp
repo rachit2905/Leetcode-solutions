cbea8635e94cf5ed6e51f10953ef06b53ec57b93
rachit2905 <85569226+rachit2905@users.noreply.github.com>
2023-01-16 17:49:07 +0530

    Time: 123 ms (67.27%), Space: 46.1 MB (24.91%) - LeetHub

---
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+     unordered_set<string> visited;
+    unordered_map<string, vector<string>> adjacent;
+    
+    void DFS(vector<string>& mergedAccount, string& email) {
+        visited.insert(email);
+        // Add the email vector that contains the current component's emails
+        mergedAccount.push_back(email);
+        
+        for (string& neighbor : adjacent[email]) {
+            if (visited.find(neighbor) == visited.end()) {
+                DFS(mergedAccount, neighbor);
+            }
+        }
+    }
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accountList) {
+         int accountListSize = accountList.size();
+        
+         for (vector<string>& account : accountList) {
+            int accountSize = account.size();
+              string accountFirstEmail = account[1];
+            for (int j = 2; j < accountSize; j++) {
+                string email = account[j];
+                adjacent[accountFirstEmail].push_back(email);
+                adjacent[email].push_back(accountFirstEmail);
+            }
+              }
+        
+        // Traverse over all th accounts to store components
+        vector<vector<string>> mergedAccounts;
+        for (vector<string>& account : accountList) {
+            string accountName = account[0];
+            string accountFirstEmail = account[1];
+              if (visited.find(accountFirstEmail) == visited.end()) {
+                vector<string> mergedAccount;
+                // Adding account name at the 0th index
+                mergedAccount.push_back(accountName);
+                DFS(mergedAccount, accountFirstEmail);
+                // Skip the first element (name)
+                // Name should be the first element, we only need to sort the emails
+                sort(mergedAccount.begin() + 1, mergedAccount.end());
+                mergedAccounts.push_back(mergedAccount);
+            }
+        }
+          return mergedAccounts;
+    }
+};

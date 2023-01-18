ab03f0984005eb1b1c39cacef202ac0ae1d1d40e
rachit2905 <85569226+rachit2905@users.noreply.github.com>
2023-01-18 15:15:20 +0530

    Time: 43 ms (49.58%), Space: 12.6 MB (95.72%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-         vector<int>prices(n,INT_MAX);
-        vector<int>temp=prices;
-        prices[src]=0;temp[src]=0;
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) 
+    {
+        vector<int>prices(n,INT_MAX),temp(n,INT_MAX);
+        prices[src]=temp[src]=0;
         for(int i=0;i<k+1;i++)
         {
-            // temp=prices;
-            for(int i=0;i<flights.size();i++)
+            for(int j=0;j<flights.size();j++)
             {
-                if(prices[flights[i][0]]!=INT_MAX)
+                if(prices[flights[j][0]]!=INT_MAX)
                 {
-                   // cout<<flights[i][1]<<"\n";
-                    if(temp[flights[i][1]]>prices[flights[i][0]]+flights[i][2])
-                        temp[flights[i][1]]=prices[flights[i][0]]+flights[i][2];
-                }
+                if(temp[flights[j][1]]>prices[flights[j][0]]+flights[j][2])
+                    temp[flights[j][1]]=prices[flights[j][0]]+flights[j][2];
+            }
             }
             prices=temp;
         }
-        if(prices[dst]==INT_MAX)return -1;
+        if(prices[dst]==INT_MAX)
+            return -1;
         else return prices[dst];
     }
 };

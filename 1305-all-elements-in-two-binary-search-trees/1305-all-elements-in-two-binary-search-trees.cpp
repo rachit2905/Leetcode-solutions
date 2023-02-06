261412d5028349be6bcd314069010f1aae00ec4d
rachit2905 <85569226+rachit2905@users.noreply.github.com>
2023-02-06 16:07:37 +0530

    Time: 182 ms (31.10%), Space: 85.2 MB (54.62%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -1,32 +1,31 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    multiset<int>s1;
-    void help(TreeNode* root)
+class Solution
+{
+    public:
+        vector<int> s1;
+    void help(TreeNode *root)
     {
-        if(!root)return;
+        if (!root) return;
         help(root->left);
-        s1.insert(root->val);
+        s1.push_back(root->val);
         help(root->right);
     }
-    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-         vector<int>ans;
+    vector<int> getAllElements(TreeNode *root1, TreeNode *root2)
+    {
+       	//vector < int>ans;
         help(root1);
         help(root2);
-        for(auto it:s1)
-        {
-            ans.push_back(it);
-        }
-        return ans;
+        sort(s1.begin(),s1.end());
+        return s1;
     }
 };

7f512b260b06a9280deabd89d95be6bb04daf04e
rachit2905 <85569226+rachit2905@users.noreply.github.com>
2023-03-11 09:51:07 +0530

    Time: 8 ms (78.35%), Space: 8 MB (88.26%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,14 +1,49 @@
 class Solution {
 public:
     int maxSatisfaction(vector<int>& satisfaction) {
-        sort(satisfaction.begin(), satisfaction.end());
+ int n = satisfaction.size();
+         sort(satisfaction.begin(),satisfaction.end());
+         int ans = 0;  // final ans to be returned
+         int index = -1;
+         for(int i =0;i<n;i++){
+             if(satisfaction[i]>=0){
+                 index = i;
+                 break;
+             }
+         }
+         // index is where first non negative element come
+
+         if(index==-1) return 0;  // all are negative so 0 
+         int pos_sum = accumulate(satisfaction.begin()+index,satisfaction.begin()+n,0);
+         // pos_sum is sum of all element which are non negative
         
-        vector<vector<int>> dp(satisfaction.size() + 1, vector<int>(satisfaction.size() + 2, 0));
-        for (int index = satisfaction.size() - 1; index >= 0; index--) {
-            for (int time = 1; time <= satisfaction.size(); time++) {
-                dp[index][time] = max(satisfaction[index] * time + dp[index + 1][time + 1], dp[index + 1][time]);
-            }
+        int multi = 1;
+        for(int i =index;i<n;i++){
+                  ans+=(multi*satisfaction[i]);
+                  multi++;
         }
-        return dp[0][1];
+        // calculated ans if we consider only non negative element
+
+
+          int cmp  = ans;
+        for(int i =index-1;i>=0;i--){
+               
+               for(int j  = index-1;j>=i;j--){
+                        cmp+=satisfaction[j];
+               }
+               cmp+=pos_sum;
+               ans = max(ans,cmp);
+        }
+        // we re going backward by one step...meaning we are taking one negative element
+        // so if we start from there each positive no will be multiplied by higher no 
+        // becoz multiplier is increased by one for all non negative so the total ans will
+        // be increased by the value equal to their sum
+        // repeating same thing taking negative element one by one and updating ans by adding the new
+        // sum due to positive element and also adding the effect of negative element
+        // after each element i am taking it's max value
+        // dry run nested for loop you will understand how it's working
+
+        return ans;
+
     }
 };

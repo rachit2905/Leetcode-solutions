6e4ab5b77f3245fdd0a68cc29aa87ba9809e4f7d
rachit2905 <85569226+rachit2905@users.noreply.github.com>
2023-04-12 16:49:10 +0530

    Time: 47 ms (26.58%), Space: 28.2 MB (60.02%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -3,47 +3,18 @@ class Solution
     public:
         int minSubArrayLen(int target, vector<int> &nums)
         {
-            int sz = 0, i = 0, j = 0, su = 0, n = nums.size();
-
-            while (j < n && su < target)
+           int sz=INT_MAX,i=0,su=0;
+            for(int j=0;j<nums.size();j++)
             {
-                su += nums[j++];
-            }
-            if (su < target) return 0;
-           // if(j==n)j--;
-            sz = j - i;
-            while (j <= n-1)
-            {
-                su += nums[j];
-                while (su > target)
+                su+=nums[j];
+                while(su>=target)
                 {
-                    if (su - nums[i] >= target)
-                    {
-                        su -= nums[i];
-                        i++;
-
-                        
-                    }
-                    else
-                        break;
-                }sz = min(sz, j - i + 1);
-                cout << i << " " << j << "\n";
-                j++;
-            }
-            if (su >= target)
-            {
-                if(j==n)j--;
-                while (su > target)
-                {
-                    if (su - nums[i] >= target)
-                    {
-                        su -= nums[i];
-                        i++;
-                    }
-                    else break;
+                    sz=min(sz,j-i+1);
+                    su-=nums[i++];
                 }
-                sz = min(sz, j - i + 1);
             }
+            if(sz!=INT_MAX)
             return sz;
+            else return 0;
         }
 };

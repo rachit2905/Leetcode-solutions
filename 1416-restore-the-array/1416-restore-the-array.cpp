6320647a8d5fbd2b36047ee5d18bf7a0e4913dd6
rachit2905 <85569226+rachit2905@users.noreply.github.com>
2023-04-23 10:31:09 +0530

    Time: 66 ms (66.86%), Space: 11.9 MB (76.74%) - LeetHub

---
+++ 1416-restore-the-array/1416-restore-the-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numberOfArrays(string s, int k) {
+          int n=s.size();
+        vector<int> dp(n+1,0);
+        dp[n]=1;
+        for(int i=n-1;i>=0;i--){
+            string str="";
+            long long res=0;
+            long x=0;
+            if(s[i]=='0') continue;
+            for(int j=i;j<min(n,i+9);j++){
+                str.push_back(s[j]);
+                x=x*10+s[j]-'0';
+                if(x>k) break;
+                res+=dp[j+1];
+                res%=(int)(1e9+7);
+            }
+            dp[i]=res;
+        }
+        return dp[0];
+    }
+};

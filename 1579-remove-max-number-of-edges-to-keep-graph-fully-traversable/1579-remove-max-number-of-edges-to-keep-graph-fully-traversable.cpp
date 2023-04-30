3b4d4fd04399a629f357ec3795b5d64bf540097b
rachit2905 <85569226+rachit2905@users.noreply.github.com>
2023-04-30 11:58:24 +0530

    Time: 543 ms (73.55%), Space: 138.9 MB (89.49%) - LeetHub

--- 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
+++ 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
@@ -1,41 +1,29 @@
 class Solution {
- int _find(int x, vector<int>& parent){
-        return parent[x] == x? x: parent[x] = _find(parent[x], parent);
-    }
 public:
+    int find(vector<int> &ds, int i) {
+    return ds[i] < 0 ? i : ds[i] = find(ds, ds[i]);
+}
     int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
-        vector<int> alice(n+1), bob(n+1);
-        for(int i=1;i<=n;i++) alice[i] = bob[i] = i;
-        sort(begin(edges), end(edges), [](auto& e1, auto& e2){
-            return e1[0]>e2[0];
-        });
-        int count = 0;
-        for(vector<int>& edge: edges){
-            bool skip = true;
-            if(edge[0] == 3 || edge[0] == 1){
-                int pa = _find(edge[1], alice);
-                int pb = _find(edge[2], alice);
-                if(pa!=pb){
-                    alice[pb] = pa;
-                    skip = false;
+    vector<int> ds_both(n + 1, -1);
+    int used = 0;
+    for (int type = 3; type > 0; --type) {
+        auto ds_one = ds_both;
+        auto &ds = type == 3 ? ds_both : ds_one;
+        for (auto &e : edges)
+            if (e[0] == type) {
+                int i = find(ds, e[1]), j = find(ds, e[2]);
+                if (i != j) {
+                    ++used;
+                    if (ds[j] < ds[i])
+                        swap(i, j);
+                    ds[i] += ds[j];
+                    ds[j] = i;
                 }
             }
-            if(edge[0] == 3 || edge[0] == 2){
-                int pa = _find(edge[1], bob);
-                int pb = _find(edge[2], bob);
-                if(pa!=pb){
-                    bob[pb] = pa;
-                    skip = false;
-                }
-            }
-            count += skip;
-        }
-        int aliceSet = _find(1, alice);
-        int bobSet = _find(1, bob);
-        for(int i=2;i<=n;i++){
-            if(_find(i, alice) != aliceSet || _find(i, bob) != bobSet)
-                return -1;
-        }
-        return count;
+        if (type != 3 && ds[find(ds, 1)] != -n)
+            return -1;
     }
+    return edges.size() - used;
+}
+
 };

ccb919da3a4899888aa0db902472e99f6bfeec9f
rachit2905 <85569226+rachit2905@users.noreply.github.com>
2023-05-09 19:10:37 +0530

    Time: 4 ms (33.67%), Space: 6.9 MB (30.23%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,41 +1,52 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-         int n=matrix.size();
-        int m=matrix[0].size();
-        int left=0,right=m-1,bottom=n-1,top=0;
-        int direction=1;
-        vector<int> v;
-        while(left<=right && top<=bottom)
-        {
-            if(direction==1)
-            {
-                for(int i=left;i<=right;i++) v.push_back(matrix[top][i]);
-                direction=2;
-                top++;
-            }
-            
-            else if(direction==2)
-            {
-                for(int i=top;i<=bottom;i++) v.push_back(matrix[i][right]);
-                direction=3;
-                right--;
-            }
-            
-            else if(direction==3)
-            {
-                for(int i=right;i>=left;i--) v.push_back(matrix[bottom][i]);
-                direction=4;
-                bottom--;
-            }
-            
-            else if(direction==4)
-            {
-                for(int i=bottom;i>=top;i--) v.push_back(matrix[i][left]);
-                direction=1;
-                left++;
-            }
+            vector<int> result;
+    if (matrix.empty()) {
+        return result;
+    }
+    
+    int m = matrix.size(); 
+    int n = matrix[0].size(); 
+    
+    int top = 0; 
+    int bottom = m - 1; 
+    int left = 0;
+    int right = n - 1; 
+    
+    int totalElements = m * n; 
+    int count = 0; 
+    
+    while (count < totalElements) {
+        for (int i = left; i <= right && count < totalElements; ++i) {
+            result.push_back(matrix[top][i]);
+            count++;
         }
-        return v;
+        top++;
+        
+        
+        for (int i = top; i <= bottom && count < totalElements; ++i) {
+            result.push_back(matrix[i][right]);
+            count++;
+        }
+        right--;
+        
+        
+        for (int i = right; i >= left && count < totalElements; --i) {
+            result.push_back(matrix[bottom][i]);
+            count++;
+        }
+        bottom--;
+        
+        
+        for (int i = bottom; i >= top && count < totalElements; --i) {
+            result.push_back(matrix[i][left]);
+            count++;
+        }
+        left++;
+    }
+    
+    return result;
+
     }
 };

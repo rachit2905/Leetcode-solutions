70977ca75a6531a6a78be1254c6bee8b55261af9
rachit2905 <85569226+rachit2905@users.noreply.github.com>
2023-06-04 11:16:58 +0530

    Time: 34 ms (18.19%), Space: 14 MB (59.39%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,40 +1,35 @@
 class Solution {
 public:
-    void dfs(int node,int parent,vector<int>adj[],vector<int>&vis)
-    {
-        vis[node]=1;
-        for(auto it:adj[node])
-        {
-            if(it==parent)continue;
-            if(!vis[it])
-            {
-                dfs(it,node,adj,vis);
-            }
-        }
-    }
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n=isConnected.size();
-        vector<int>adj[n+1];
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(isConnected[i][j])
-                {
-                    adj[i+1].push_back(j+1);
-                    adj[j+1].push_back(i+1);
+    void bfs(int node, vector<vector<int>>& isConnected, vector<bool>& visit) {
+        queue<int> q;
+        q.push(node);
+        visit[node] = true;
+
+        while (!q.empty()) {
+            node = q.front();
+            q.pop();
+
+            for (int i = 0; i < isConnected.size(); i++) {
+                if (isConnected[node][i] && !visit[i]) {
+                    q.push(i);
+                    visit[i] = true;
                 }
             }
         }
-        vector<int>vis(n+1,0);int ans=0;
-        for(int i=1;i<=n;i++)
-        {
-            if(!vis[i])
-            {
-                dfs(i,-1,adj,vis);
-                ans++;
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        int numberOfComponents = 0;
+        vector<bool> visit(n);
+
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                numberOfComponents++;
+                bfs(i, isConnected, visit);
             }
         }
-        return ans;
+
+        return numberOfComponents;
     }
 };

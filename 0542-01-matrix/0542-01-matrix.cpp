c2d7fc59397e8cee016e9e591edeaaf1e5d40abf
rachit2905 <85569226+rachit2905@users.noreply.github.com>
2023-08-17 11:45:24 +0530

    Time: 68 ms (82.37%), Space: 34.6 MB (17.65%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,40 +1,35 @@
 class Solution {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-         queue<pair<pair<int,int>,int>> q;
-        int n=mat.size();
-        int m=mat[0].size();
-
-        vector<vector<int>> ans(n,vector<int>(m,0));
-        vector<vector<int>>visited(n,vector<int>(m,0));
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(mat[i][j]==0){
+      vector<vector<int>> updateMatrix(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> vis(m,vector<int> (n,0));
+        vector<vector<int>> ans(m,vector<int> (n,0));
+        queue<pair<pair<int,int>,int>> q;    //{row,col},dist
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j] == 0){
                     q.push({{i,j},0});
-                    visited[i][j]=1;
+                    vis[i][j] = 1;
                 }
             }
         }
- int drow[]={+1,0,-1,0};
-        int dcol[]={0,+1,0,-1};
-
+        int drow[] = {-1,0,1,0};
+        int dcol[] = {0,1,0,-1};
         while(!q.empty()){
-            int row=q.front().first.first;
-            int col=q.front().first.second;
-            int steps=q.front().second;
-            ans[row][col]=steps;
+            auto it = q.front();
+            int row = it.first.first;
+            int col = it.first.second;
+            int dist = it.second;
             q.pop();
-
+            ans[row][col] = dist;
             for(int i=0;i<4;i++){
-                int nrow=row+drow[i];
-            int ncol=col+dcol[i];
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && visited[nrow][ncol]==0){
-                visited[nrow][ncol]=1;
-                q.push({{nrow,ncol},steps+1});
-            }
-
-
+                int nrow = row + drow[i];
+                int ncol = col + dcol[i];
+                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && !vis[nrow][ncol]){
+                    q.push({{nrow,ncol},dist+1});
+                    vis[nrow][ncol] = 1;
+                }
             }
         }
         return ans;

fe83afb50fcfe46363b8b09a0f3bd2527ac9ae82
rachit2905 <85569226+rachit2905@users.noreply.github.com>
2023-08-31 12:09:33 +0530

    Added solution - LeetHub

---
+++ Number of Longest Increasing Subsequence - GFG/number-of-longest-increasing-subsequence.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int NumberofLIS(int n, vector<int>&arr) {
+        // Code here
+         //int n = arr.size();
+    
+    vector<int> dp(n,1);
+    vector<int> ct(n,1);
+    
+    int maxi = 1;
+    
+    for(int i=0; i<=n-1; i++){
+        for(int prev_index = 0; prev_index <=i-1; prev_index ++){
+            
+            if(arr[prev_index]<arr[i] && dp[prev_index]+1>dp[i]){
+                dp[i] = dp[prev_index]+1;
+                //inherit
+                ct[i] = ct[prev_index];
+            }
+            else if(arr[prev_index]<arr[i] && dp[prev_index]+1==dp[i]){
+                //increase the count
+                ct[i] = ct[i] + ct[prev_index];
+            }
+        }
+         maxi = max(maxi,dp[i]);
+    }
+    
+    int nos =0;
+    
+    for(int i=0; i<=n-1; i++){
+       if(dp[i]==maxi) nos+=ct[i];
+    }
+    
+    return nos; 
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n; cin>>n;
+        vector<int> arr;
+        
+        for(int i=0; i<n; ++i){
+            long long x; cin>>x;
+            arr.push_back(x);
+        }
+        
+        Solution obj;
+        cout<<obj.NumberofLIS(n, arr)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends

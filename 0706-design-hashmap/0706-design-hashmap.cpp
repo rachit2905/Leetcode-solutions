1ff260e7551f4b88c67100987ccfe3c935b0784e
rachit2905 <85569226+rachit2905@users.noreply.github.com>
2023-10-04 10:27:18 +0530

    Time: 101 ms (82.50%), Space: 62.3 MB (47.70%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.cpp
+++ 0706-design-hashmap/0706-design-hashmap.cpp
@@ -1,30 +1,65 @@
-class MyHashMap {
+struct KeyValue {
+    int key;
+    int value;
+
 public:
-    map<int,int>m1;
+    KeyValue(int _key, int _value):key(_key), value(_value) {}
+};
+
+
+class MyHashMap {
+
+    vector<list<KeyValue>> bucket;
+    int bucketSize; 
+
+    list<KeyValue>::iterator find(int key)
+    {
+        int index = hash(key);
+        for(auto iter = bucket[index].begin(); iter != bucket[index].end(); iter++)
+        {
+            if(iter->key == key)
+                return iter;
+        }
+        return bucket[index].end();
+    }
+
+    int hash(int key)
+    {
+        return  key% bucketSize;
+    }
+
+public:
     MyHashMap() {
-        
+        bucketSize = 10007;
+        bucket.resize(bucketSize);
     }
     
     void put(int key, int value) {
-        m1[key]=value;
+        
+        auto iter = find(key);
+        int index = hash(key);
+        if(iter != bucket[index].end())
+        {
+            iter->value = value;
+            return;
+        }
+        bucket[index].push_back(KeyValue(key, value));
     }
     
     int get(int key) {
-        if(m1.find(key)!=m1.end())
-        return m1[key];
+        auto iter = find(key);
+        int index = hash(key);
+        if(iter != bucket[index].end())
+            return iter->value;
         return -1;
     }
     
     void remove(int key) {
-          if(m1.find(key)!=m1.end())
-        m1.erase(m1.find(key));
+        
+        auto iter = find(key);
+        int index = hash(key);
+        if(iter != bucket[index].end())
+            bucket[index].erase(iter);;
+
     }
 };
-
-/**
- * Your MyHashMap object will be instantiated and called as such:
- * MyHashMap* obj = new MyHashMap();
- * obj->put(key,value);
- * int param_2 = obj->get(key);
- * obj->remove(key);
- */

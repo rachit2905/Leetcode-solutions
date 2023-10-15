52a765e8d126d1c515c735c40f4fa68d380e71b5
rachit2905 <85569226+rachit2905@users.noreply.github.com>
2023-10-15 11:10:59 +0530

    Time: 25 ms (47.40%), Space: 10.5 MB (61.25%) - LeetHub

--- 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
-     const int MOD = 1000000007;
-    const int MAX_LEN = 251;
-
+    const int mod=1e9+7;
     vector<vector<int>> dp;
+       int f(int steps, int i, int n) {//(remaining moves, index, n)
+        if (steps==0) 
+            return (i==0) ? 1 : 0;
 
-    int recurse(int steps, int arrLen, int pos) {
-        if (steps == 0) return pos == 0 ? 1 : 0;
-        if (dp[steps][pos] != -1) return dp[steps][pos];
-        long long left = 0;
-        if (pos > 0) left = recurse(steps-1, arrLen, pos-1) % MOD;
-        long long right = 0;
-        if (pos < arrLen - 1) right = recurse(steps-1, arrLen, pos+1) % MOD;
-        long long stay = recurse(steps-1, arrLen, pos) % MOD;
-        return dp[steps][pos] = (left + right + stay) % MOD;
+        if (dp[steps][i]!= -1) 
+            return dp[steps][i];
+
+        long long ans=f(steps-1, i, n) %mod;//stay
+
+        // Move to the right 
+        if (i<n-1) 
+            ans= (ans+f(steps-1, i+1, n))%mod;
+
+        // Move to the left 
+        if (i>0) 
+            ans = (ans+f(steps-1, i-1, n))%mod;
+         
+        return dp[steps][i]=ans;
     }
-public:
+
     int numWays(int steps, int arrLen) {
-        dp = vector(steps+1, vector(MAX_LEN, -1));
-        return recurse(steps, min(arrLen, MAX_LEN), 0) % MOD;
+        int n=min(steps/2+1, arrLen);//maximial position can reached
+        dp.assign(steps+1, vector<int>(n+1, -1));
+        return f(steps, 0, n);
     }
 };

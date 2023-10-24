47806c1a9499a1eed0ce6354036361e0288ba398
rachit2905 <85569226+rachit2905@users.noreply.github.com>
2023-10-24 16:01:44 +0530

    Time: 12 ms (96.45%), Space: 58.1 MB (64.94%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -1,37 +1,24 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    TreeNode* x = NULL, *y = NULL, *pred = NULL;
+	TreeNode* firstMistake, *secondMistake, *pre;
+	void recoverTree(TreeNode* root) {
+		pre = new TreeNode(INT_MIN);
+		inorder(root);
+		swap(firstMistake->val, secondMistake->val);
+	}
 
-  void swap(TreeNode* a, TreeNode* b) {
-    int tmp = a->val;
-    a->val = b->val;
-    b->val = tmp;
-  }
+	void inorder(TreeNode* root) {
+		if(root == nullptr) 
+			return;
 
-  void findTwoSwapped(TreeNode* root) {
-    if (root == NULL) return;
-    findTwoSwapped(root->left);
-    if (pred != NULL && root->val < pred->val) {
-      y = root;
-      if (x == NULL) x = pred;
-      else return;
-    }
-    pred = root;
-    findTwoSwapped(root->right);
-  }
-    void recoverTree(TreeNode* root) {
-       findTwoSwapped(root);
-    swap(x, y); 
-    }
+		inorder(root->left);
+
+		if(firstMistake == nullptr && root->val < pre->val)
+			firstMistake = pre;
+		if(firstMistake != nullptr && root->val < pre->val)
+			secondMistake = root;
+		pre = root;
+
+		inorder(root->right);
+	}
 };
